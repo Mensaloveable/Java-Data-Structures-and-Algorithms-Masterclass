880d120e586e7b4415e51dada03275a5844d4150
Mensaloveable <abdullahi.adedayo@gmail.com>
2023-01-23 08:29:56 +0100

    Decimal To Binary

--- src/main/java/com/loveable/recursion/interviewQuestion/DecimalToBinary.java
+++ src/main/java/com/loveable/recursion/interviewQuestion/DecimalToBinary.java
@@ -4,7 +4,6 @@ public class DecimalToBinary {
     public static Integer decimalToBinary(int decimal) {
         if(decimal == 0)
             return 0;
-        System.out.println(decimal  + " and " + decimal%2);
         return (decimal % 2 ) + 10  * decimalToBinary(decimal/2);
     }
 }
