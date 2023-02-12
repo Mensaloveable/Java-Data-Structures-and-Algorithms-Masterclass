466730f277124a7b6fe1e7edb5d7dae5ffed69ad
Mensaloveable <abdullahi.adedayo@gmail.com>
2023-02-12 16:43:35 +0100

    Recursion Exercises

--- src/main/java/com/loveable/Main.java
+++ src/main/java/com/loveable/Main.java
@@ -1,48 +1,20 @@
 package com.loveable;
 
-import java.util.ArrayList;
-import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-
-
+        int i = countWays(3);
+        System.out.println(i);
     }
 
-    class Pair {
-        int x;
-        int y;
-        Pair(int x, int y) {
-            this.x = x;
-            this.y = y;
+    public static int countWays(int n) {
+        if (n < 0) {
+            return 0;
+        } else if (n == 0) {
+            return 1;
+        } else {
+            return countWays(n-1) + countWays(n-2) + countWays(n-3);
         }
-    }
-
-    public ArrayList<Pair> findClosestPair(int[] arr) {
-        int minDiff = Integer.MAX_VALUE;
-        ArrayList<Pair> closestPairs = new ArrayList<>();
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < arr.length; i++) {
-            map.put(arr[i], i);
-        }
-        for (int i = 0; i < arr.length; i++) {
-            int curr = arr[i];
-            int closest = Integer.MAX_VALUE;
-            for (int j = -1; j <= 1; j++) {
-                int diff = curr + j;
-                if (map.containsKey(diff) && Math.abs(diff - curr) < closest) {
-                    closest = Math.abs(diff - curr);
-                    int index = map.get(diff);
-                    if (index != i && closest < minDiff) {
-                        minDiff = closest;
-                        closestPairs.clear();
-                        closestPairs.add(new Pair(curr, diff));
-                    }
-                }
-            }
-        }
-        return closestPairs;
     }
 }
