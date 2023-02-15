2f1ead3c54ed134011d918867c00fbaf5202541f
Mensaloveable <abdullahi.adedayo@gmail.com>
2023-02-15 23:00:18 +0100

    isUnique (String)

--- src/main/java/com/loveable/string/interviewQuestion/isUnique.java
+++ src/main/java/com/loveable/string/interviewQuestion/isUnique.java
@@ -2,7 +2,20 @@ package com.loveable.string.interviewQuestion;
 
 public class isUnique {
     /**
-     * @Question: Is Unique: Implement an algorithm to determine if a string has all unique characters.
-     * @Try What if you cannot use additional data structures?
+     * {@code @Question:} Is Unique: Implement an algorithm to determine if a string has all unique characters.
+     * <p>
+     * {@code @Try} What if you cannot use additional data structures?
      */
+    public static boolean unique(String word) {
+        char[] charArray = word.toCharArray();
+        for (int i = 0; i < charArray.length; i++) {
+            for (int j = 0; j < charArray.length; j++) {
+                if (i == j)
+                    continue;
+                if (charArray[i] == charArray[j])
+                    return false;
+            }
+        }
+        return true;
+    }
 }
