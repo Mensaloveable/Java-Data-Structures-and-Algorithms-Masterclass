b7461a02e0d8072eeea378a6fea9d934a04942ea
Mensaloveable <abdullahi.adedayo@gmail.com>
2023-02-18 06:59:15 +0100

    Create addFirst method

--- src/main/java/com/loveable/linkedList/Linked_List.java
+++ src/main/java/com/loveable/linkedList/Linked_List.java
@@ -4,7 +4,7 @@ public class Linked_List<T> {
 
     public static class Node<T> {
         T data;
-        Node<T> node;
+        Node<T> next;
 
         public Node(T data) {
             this.data = data;
@@ -14,8 +14,29 @@ public class Linked_List<T> {
         public String toString() {
             return "Node{" +
                     "data=" + data +
-                    ", node=" + node +
+                    ", node=" + next +
                     '}';
         }
+    }
+
+    private Node<T> head;
+    private Node<T> tail;
+
+    public void addFirst(T element) {
+        Node<T> newNode = new Node<>(element);
+        if (head == null)
+            head = tail = newNode;
+        else {
+            newNode.next = head;
+            head = newNode;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Linked_List{" +
+                "head=" + head +
+                ", tail=" + tail +
+                '}';
     }
 }
