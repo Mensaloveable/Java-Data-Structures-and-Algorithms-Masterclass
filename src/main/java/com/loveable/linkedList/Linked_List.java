fb101e99b7286441ec15c1eea8e93671ca9e3683
Mensaloveable <abdullahi.adedayo@gmail.com>
2023-02-19 09:43:47 +0100

    Create removeLast

--- src/main/java/com/loveable/linkedList/Linked_List.java
+++ src/main/java/com/loveable/linkedList/Linked_List.java
@@ -66,6 +66,24 @@ public class Linked_List<T> {
         size--;
     }
 
+    public void removeLast() {
+        if (head == null)
+            throw new NoSuchElementException();
+        Node<T> node = head;
+        if (node.next == null) {
+            head = tail = null;
+            size--;
+            return;
+        }
+        while (node.next.next != null) {
+            node = node.next;
+        }
+        node.next = null;
+        tail.next = node;
+        tail = node;
+        size--;
+    }
+
     public int size() {
         return size;
     }
